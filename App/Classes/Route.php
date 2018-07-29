3de47ee683f67f9a2908bbf93588c745899861db
Gustavo Rizzon <gustavop.rizzon@gmail.com>
2018-07-28 23:34:46 -0300

    now we can define dynamic routes, where the dynamic values are passed to their respective handlers as arguments.

--- App/Classes/Route.php
+++ App/Classes/Route.php
@@ -8,26 +8,82 @@ class Route
 {
 	public static function check (string $route, $handler)
 	{
-		if ($route == Request::getRequestedURI()) {
-			if (is_callable($handler)) {
-				$handler();
-			} else if (is_string($handler)) {
-				self::controller($handler);
+		$parseResult = self::parse($route);
+		if ($parseResult['run']) {
+			if ($parseResult['isDynamic']) {
+				self::run($handler, $parseResult['args']);
 			} else {
-				throw new \Exception('The route handler is not valid! Must be a function or a controller string.');
+				self::run($handler);
 			}
-			exit();
+			self::terminate();
 		}
 	}
 
-	private static function controller (string $controllerString)
+	private static function parse (string $route)
+	{
+		$run = false;
+		$isDynamic = false;
+		$args = [];
+		if ($route == Request::getRequestedURI()) {
+			$run = true;
+		} else {
+			if (strpos($route, '{')) {
+				$route = explode('/', $route);
+				$uri = Request::getRequestedURIExploded();
+				$routeCount = count($route);
+				if ($routeCount == count($uri)) {
+					$run = true;
+					$isDynamic = true;
+					for ($i = 1; $i < $routeCount; $i++) {
+						if ($route[$i] != $uri[$i]) {
+							preg_match('/(?<=\{).+?(?=\})/', $route[$i], $match);
+							if (empty($match)) {
+								$run = false;
+								break;
+							} else {
+								$args[$match[0]] = $uri[$i];
+							}
+						}
+					}
+				}
+			}
+		}
+		return [
+			'run' => $run,
+			'isDynamic' => $isDynamic,
+			'args' => $args
+		];
+	}
+
+	private static function run ($handler, array $args = [])
+	{
+		if (is_callable($handler)) {
+			$handler($args);
+		} else if (is_string($handler)) {
+			$controller = self::prepareControllerCall($handler);
+			self::callController($controller[0], $controller[1], $args);
+		} else {
+			throw new \Exception('The route handler is not valid! Must be a function or a controller string.');
+		}
+	}
+
+	private static function prepareControllerCall (string $controllerString)
 	{
 		$controllerArray = explode('@', $controllerString);
 		if (count($controllerArray) != 2) {
 			throw new \Exception('The entered string is not in the proper format to continue execution.');
 		}
-		$controller = 'App\\Controllers\\' . str_replace('/', '\\', $controllerArray[0]);
-		$method = $controllerArray[1];
-		$controller::$method();
+		$controllerArray[0] = 'App\\Controllers\\' . str_replace('/', '\\', $controllerArray[0]);
+		return $controllerArray;
+	}
+
+	private static function callController (string $controllerPath, string $methodName, array $args)
+	{
+		$controllerPath::$methodName($args);
+	}
+
+	private static function terminate ()
+	{
+		exit();
 	}
 }
